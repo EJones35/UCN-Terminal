52b6faffe2cbac555c60bdda4397ebbfda93bfd9
Ethan Jones <smidge@garyjones.co.uk>
2026-06-28 19:48:54 +0100

    Initialize Firebase app and services
    Set up Firebase configuration and initialized Firestore and Auth services.

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { 
+  getAuth,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCP57X_6iOxam8QyHMJG_8l2xbEuh38oxk",
+  authDomain: "ucn-terminal.firebaseapp.com",
+  projectId: "ucn-terminal",
+  storageBucket: "ucn-terminal.firebasestorage.app",
+  messagingSenderId: "871960027081",
+  appId: "1:871960027081:web:f9287fd87fb7309888d218"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// SESSION PERSISTENCE (critical)
+setPersistence(auth, browserLocalPersistence);
