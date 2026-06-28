7e91ea5af23aa9fdc4f88c462c3f52f2d4dd2acb
Ethan Jones <smidge@garyjones.co.uk>
2026-06-28 22:09:50 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,10 +3,10 @@ import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gs
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP57X_6iOxam8QyHMJG_8l2xbEuh38oxk",
+  apiKey: "",
   authDomain: "ucn-terminal.firebaseapp.com",
   projectId: "ucn-terminal",
-  appId: "1:871960027081:web:f9287fd87fb7309888d218"
+  appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
