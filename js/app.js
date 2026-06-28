244cfdca70c945aa2ecca39f9bc808ba9e7ee7ee
Ethan Jones <smidge@garyjones.co.uk>
2026-06-28 19:50:55 +0100

    Implement user authentication redirect logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
+const go = (page) => window.location.href = page;
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    go("home.html");
+  } else {
+    go("auth.html");
+  }
+});
