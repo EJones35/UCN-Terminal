dd378eb1a9456b9b256957ea679ea7ef8e46dde4
Ethan Jones <smidge@garyjones.co.uk>
2026-06-29 07:43:00 +0100

    Update Firebase configuration with new credentials

--- js/firebase.js
+++ js/firebase.js
@@ -9,10 +9,10 @@ import {
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCP57X_6iOxam8QyHMJG_8l2xbEuh38oxk",
+  authDomain: "ucn-terminal.firebaseapp.com",
+  projectId: "ucn-terminal",
+  appId: "1:871960027081:web:f9287fd87fb7309888d218"
 };
 
 const app = initializeApp(firebaseConfig);
